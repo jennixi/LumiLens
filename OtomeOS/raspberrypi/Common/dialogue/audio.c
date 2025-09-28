f64de729d04221bdefdbb2f5c3b85c4174dcaf3d
jen <130325520+jennixi@users.noreply.github.com>
2025-09-28 08:25:26 -0400

    Delete OtomeOS/raspberrypi directory
    unused

--- OtomeOS/raspberrypi/Common/dialogue/audio.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int record_audio(const char* path){
-  char cmd[256];
-  snprintf(cmd,sizeof(cmd),"arecord -d 4 -f cd -t wav %s >/dev/null 2>&1", path);
-  return system(cmd);
-}
--- OtomeOS/raspberrypi/Common/face/camera.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int take_photo(const char* path){
-  char cmd[256];
-  snprintf(cmd,sizeof(cmd),"libcamera-still -n -o %s --width 640 --height 480 >/dev/null 2>&1", path);
-  return system(cmd);
-}
