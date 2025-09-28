81a4437863fdd8df27b434a78ac497e1d8f03d8f
jennixi <jennicoddi@gmail.com>
2025-09-28 05:27:43 -0400

    Add modified Facial-Emotion-Recognition-with-OpenCV-and-Deepface (from https://github.com/original-owner/repo)

---
+++ LumiLensApp/LumiLens/LumiLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiLensAppApp.swift
+//  LumiLensApp
+//
+//  Created by bengal on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct LumiLensAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
